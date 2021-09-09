3a946b0582024685f39272c8d1afb5ce8585810b
khaled <khaled@gtlogic.com>
2021-09-09 17:19:04 +0300

    DONE

--- server.py
+++ server.py
@@ -9,30 +9,20 @@ import tensorflow_recommenders as tfrs
 import matplotlib.pyplot as plt
 import tensorflowjs as tfjs
 
-# import urlparse
-
 def train():
   url = "train.csv"
   plans_df = pd.read_csv(url, usecols=["PlanName"], dtype=str).drop_duplicates()
   employees_df = pd.read_csv(url, dtype=str, usecols=["EmployeeId", "eemGender", "eemBirthDate", "eemState"]).drop_duplicates()
-  enrollment_df = pd.read_csv(url, dtype=str, usecols=["eemState", "PlanName", "eemGender"])
-  enrollment_df = enrollment_df.groupby(["PlanName", "eemGender", "eemState"]).size().reset_index(name="count")
+  enrollment_df = pd.read_csv(url, dtype=str, usecols=["eemState", "PlanName", "eemGender", "eemZipCode"])
+  enrollment_df = enrollment_df.groupby(["PlanName", "eemGender", "eemState", "eemZipCode"]).size().reset_index(name="count")
 
   enrollment_ds = tf.data.Dataset.from_tensor_slices(dict(enrollment_df))
-
-  # enrollment_ds = (
-  #   tf.data.Dataset.from_tensor_slices(
-  #     (
-  #       enrollment_df[feature_names].values,
-  #       enrollment_df['count'].values
-  #     )
-  #   )
-  # )
 
   enrollment_ds = enrollment_ds.map(lambda x: {
     "count": x["count"],
     "gender": x["eemGender"],
     "state": x["eemState"],
+    "zip_code": x["eemZipCode"],
     "plan_name": x["PlanName"]
   })
   print(enrollment_df)
@@ -40,11 +30,10 @@ def train():
   tf.random.set_seed(42)
   shuffled = enrollment_ds.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
 
-  # // change numbers
   train = shuffled.take(10000)
   test = shuffled.skip(10000).take(20_000)
 
-  feature_names = ["plan_name", "gender", "state"]
+  feature_names = ["plan_name", "gender", "state", "zip_code"]
 
   vocabularies = {}
 
@@ -59,7 +48,7 @@ def train():
 
       self.embedding_dimension = 32
 
-      str_features = ["plan_name", "gender", "state"]
+      str_features = ["plan_name", "gender", "state", "zip_code"]
       int_features = []
 
       self._all_features = str_features + int_features
@@ -109,7 +98,6 @@ def train():
         embedding_fn = self._embeddings[feature_name]
         embeddings.append(embedding_fn(features[feature_name]))
       x = tf.keras.layers.Concatenate(axis=1)(embeddings)
-      # x = tf.concat(embeddings, axis=1)
 
       # Build Cross Network
       if self._cross_layer is not None:
@@ -132,44 +120,8 @@ def train():
   cached_train = train.shuffle(100_000).batch(8192).cache()
   cached_test = test.batch(4096).cache()
 
-  # def run_models(use_cross_layer, deep_layer_sizes, projection_dim=None, num_runs=5):
-  #   models = []
-  #   rmses = []
-
-  #   for i in range(num_runs):
-  #     model = DCN(use_cross_layer=use_cross_layer,
-  #                 deep_layer_sizes=deep_layer_sizes,
-  #                 projection_dim=projection_dim)
-  #     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate))
-  #     models.append(model)
-
-  #     model.fit(cached_train, epochs=epochs, verbose=False)
-  #     metrics = model.evaluate(cached_test, return_dict=True)
-  #     rmses.append(metrics["RMSE"])
-
-  #   mean, stdv = np.average(rmses), np.std(rmses)
-
-  #   return {"model": models, "mean": mean, "stdv": stdv}
-
   epochs = 8
   learning_rate = 0.01
-
-  # dcn_result = run_models(use_cross_layer=True, deep_layer_sizes=[192, 192])
-
-  # dcn_lr_result = run_models(use_cross_layer=True,
-  #                            projection_dim=20,
-  #                            deep_layer_sizes=[192, 192])
-
-  # dnn_result = run_models(use_cross_layer=False,
-  #                         deep_layer_sizes=[192, 192, 192])
-
-
-  # print("DCN            RMSE mean: {:.4f}, stdv: {:.4f}".format(
-  #     dcn_result["mean"], dcn_result["stdv"]))
-  # print("DCN (low-rank) RMSE mean: {:.4f}, stdv: {:.4f}".format(
-  #     dcn_lr_result["mean"], dcn_lr_result["stdv"]))
-  # print("DNN            RMSE mean: {:.4f}, stdv: {:.4f}".format(
-  #     dnn_result["mean"], dnn_result["stdv"]))
 
 
   model = DCN(use_cross_layer=False,
@@ -181,6 +133,8 @@ def train():
   return model
 
 class handler(BaseHTTPRequestHandler):
+    recommendationModel = None
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
@@ -193,19 +147,16 @@ class handler(BaseHTTPRequestHandler):
         print(details)
         detailsJSON = json.loads(details)
 
-        recommendationModel = train()
-        print(recommendationModel)
-        print(detailsJSON)
-        # recommendationModel = tf.saved_model.load("export")
-        print('details.plans')
-        print(detailsJSON["planNames"])
-        print(detailsJSON["gender"])
+        if(handler.recommendationModel == None):
+            handler.recommendationModel = train()
+
         weight_by_plan = {}
         for plan_name in detailsJSON["planNames"]:
-            weight_by_plan[plan_name] = recommendationModel({
+            weight_by_plan[plan_name] = handler.recommendationModel({
                 "plan_name": np.array([plan_name]),
                 "gender": np.array([detailsJSON["gender"]]),
-                "state": np.array([detailsJSON["state"]])
+                "state": np.array([detailsJSON["state"]]),
+                "zip_code": np.array([detailsJSON["zipCode"]])
             })
 
         array = sorted(weight_by_plan.items(), key=lambda x: x[1], reverse=True)
@@ -213,14 +164,8 @@ class handler(BaseHTTPRequestHandler):
 
         detailsJSON = json.loads(details)
 
-        # print(type(query))
-        # print(u"[START]: Received GET for %s with query: %s" % (path, detailsJSON))
-        # self.wfile.write({ "recommended": recommended })
         self.wfile.write(bytes(recommended, "utf8"))
-        # return json.dumps({"recommended": recommended})
-        # self.wfile.write(json.dumps({"recommended": recommended}))
 
-        print(type(query))
         print(u"[START]: Received GET for %s with query: %s" % (path, detailsJSON))
 
 with HTTPServer(('', 7800), handler) as server:
