caa845b16b9e7d07a1cbebf48386414101cdfbea
khaled <khaled@gtlogic.com>
2021-09-09 08:26:56 +0300

    add the changes

--- train-updated.py
+++ train-updated.py
@@ -1,104 +1,178 @@
 import numpy as np
-from typing import Dict, Text
 import pandas as pd
-# Make numpy values easier to read.
-np.set_printoptions(precision=3, suppress=True)
-
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 import tensorflow_recommenders as tfrs
-import pprint;
+import matplotlib.pyplot as plt
+import tensorflowjs as tfjs
 
-coverages_url = "coverages.csv"
-coverages_df = pd.read_csv(coverages_url, dtype = str)
-coverages_dataset = tf.data.Dataset.from_tensor_slices(dict(coverages_df))
-coverages_dataset = coverages_dataset.map(lambda x: x["ID"])
+url = "train.csv"
+plans_df = pd.read_csv(url, usecols=["PlanName"], dtype=str).drop_duplicates()
+employees_df = pd.read_csv(url, dtype=str, usecols=["EmployeeId", "eemGender", "eemBirthDate", "eemState"]).drop_duplicates()
+enrollment_df = pd.read_csv(url, dtype=str, usecols=["eemState", "PlanName", "eemGender"])
+enrollment_df = enrollment_df.groupby(["PlanName", "eemGender", "eemState"]).size().reset_index(name="count")
 
+enrollment_ds = tf.data.Dataset.from_tensor_slices(dict(enrollment_df))
 
-employees_url = "employees.csv"
-employees_df = pd.read_csv(employees_url, dtype = str)
-employees_dataset = tf.data.Dataset.from_tensor_slices(dict(employees_df))
-employees_dataset = employees_dataset.map(lambda x: {
-    "ID": x["ID"],
-    "coverage_id": x["Coverage ID"],
-    "age": x["Age"],
-    "gender": x["Gender"]
+enrollment_ds = enrollment_ds.map(lambda x: {
+   "count": x["count"],
+   "eemGender": x["eemGender"],
+   "eemState": x["eemState"], 
+   "PlanName": x["PlanName"] 
 })
+print(enrollment_df)
 
-for x in employees_dataset.take(1).as_numpy_iterator():
-    pprint.pprint(x)
+tf.random.set_seed(42)
+shuffled = enrollment_ds.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
 
-# employee_ids_vocabulary = tf.keras.layers.experimental.preprocessing.StringLookup(mask_token=None)
-# employee_ids_vocabulary.adapt(employees_dataset.map(lambda x: x["ID"]))
+# // change numbers
+train = shuffled.take(10000)
+test = shuffled.skip(10000).take(20_000)
 
-# coverage_ids_vocabulary = tf.keras.layers.experimental.preprocessing.StringLookup(mask_token=None)
-# coverage_ids_vocabulary.adapt(coverages_dataset)
+feature_names = ["PlanName", "eemGender", "eemState"]
 
-# employee_model = tf.keras.Sequential([
-#     employee_ids_vocabulary,
-#     tf.keras.layers.Embedding(employee_ids_vocabulary.vocabulary_size(), 64)
-# ])
-# coverage_model = tf.keras.Sequential([
-#     coverage_ids_vocabulary,
-#     tf.keras.layers.Embedding(coverage_ids_vocabulary.vocabulary_size(), 64)
-# ])
+vocabularies = {}
 
-# # Define your objectives.
-# task = tfrs.tasks.Retrieval(metrics=tfrs.metrics.FactorizedTopK(
-#     coverages_dataset.batch(2).map(coverage_model)
-#   )
-# )
+for feature_name in feature_names:
+  vocab = enrollment_ds.batch(1_000_000).map(lambda x: x[feature_name])
+  vocabularies[feature_name] = np.unique(np.concatenate(list(vocab)))
 
-# class CoverageModel(tfrs.Model):
+class DCN(tfrs.Model):
 
-#   def __init__(
-#       self,
-#       coverage_model: tf.keras.Model,
-#       employee_model: tf.keras.Model,
-#       task: tfrs.tasks.Retrieval):
-#     super().__init__()
+  def __init__(self, use_cross_layer, deep_layer_sizes, projection_dim=None):
+    super().__init__()
 
-#     self.coverage_model = coverage_model
-#     self.employee_model = employee_model
+    self.embedding_dimension = 32
 
-#     # Set up a retrieval task.
-#     self.task = task
+    str_features = ["PlanName", "eemState", "eemGender"]
+    int_features = []
 
-#   def compute_loss(self, features: Dict[Text, tf.Tensor], training=False) -> tf.Tensor:
-#     # Define how the loss is computed.
+    self._all_features = str_features + int_features
+    self._embeddings = {}
 
-#     employee_embeddings = self.employee_model(features["ID"])
-#     # query_embeddings = self.query_model({
-#     #     "ID": features["ID"],
-#     #     ""
-#     # })
-#     coverage_embeddings = self.coverage_model(features["coverage_id"])
+    # Compute embeddings for string features.
+    for feature_name in str_features:
+      vocabulary = vocabularies[feature_name]
+      self._embeddings[feature_name] = tf.keras.Sequential(
+          [tf.keras.layers.experimental.preprocessing.StringLookup(
+              vocabulary=vocabulary, mask_token=None),
+           tf.keras.layers.Embedding(len(vocabulary) + 1,
+                                     self.embedding_dimension)
+    ])
 
-#     return self.task(employee_embeddings, coverage_embeddings)
+    # Compute embeddings for int features.
+    for feature_name in int_features:
+      vocabulary = vocabularies[feature_name]
+      self._embeddings[feature_name] = tf.keras.Sequential(
+          [tf.keras.layers.experimental.preprocessing.IntegerLookup(
+              vocabulary=vocabulary, mask_value=None),
+           tf.keras.layers.Embedding(len(vocabulary) + 1,
+                                     self.embedding_dimension)
+    ])
 
-# model = CoverageModel(employee_model, coverage_model, task)
-# model.compile(optimizer=tf.keras.optimizers.Adagrad(0.5))
-# # Train for 3 epochs.
-# model.fit(employees_dataset.batch(4), epochs=3)
+    if use_cross_layer:
+      self._cross_layer = tfrs.layers.dcn.Cross(
+          projection_dim=projection_dim,
+          kernel_initializer="glorot_uniform")
+    else:
+      self._cross_layer = None
 
-# # Use brute-force search to set up retrieval using the trained representations.
-# index = tfrs.layers.factorized_top_k.BruteForce(model.employee_model)
-# index.index_from_dataset(
-#     coverages_dataset.batch(4).map(lambda ID: (ID, model.coverage_model(ID))))
+    self._deep_layers = [tf.keras.layers.Dense(layer_size, activation="relu")
+      for layer_size in deep_layer_sizes]
 
-# # Get some recommendations.
-# _, titles = index(np.array(["1"]))
-# print(f"Top 3 recommendations for user 42: {titles[0, :3]}")
-# # model.build(employees_df.shape)
-# # model.save("model")
+    self._logit_layer = tf.keras.layers.Dense(1)
+
+    self.task = tfrs.tasks.Ranking(
+      loss=tf.keras.losses.MeanSquaredError(),
+      metrics=[tf.keras.metrics.RootMeanSquaredError("RMSE")]
+    )
+
+  def call(self, features):
+    # Concatenate embeddings
+    embeddings = []
+    for feature_name in self._all_features:
+      embedding_fn = self._embeddings[feature_name]
+      embeddings.append(embedding_fn(features[feature_name]))
+
+    x = tf.concat(embeddings, axis=1)
+
+    # Build Cross Network
+    if self._cross_layer is not None:
+      x = self._cross_layer(x)
+
+    # Build Deep Network
+    for deep_layer in self._deep_layers:
+      x = deep_layer(x)
+
+    return self._logit_layer(x)
+
+  def compute_loss(self, features, training=False):
+    labels = features.pop("count")
+    scores = self(features)
+    return self.task(
+        labels=labels,
+        predictions=scores,
+    )
+
+cached_train = train.shuffle(100_000).batch(8192).cache()
+cached_test = test.batch(4096).cache()
+
+# def run_models(use_cross_layer, deep_layer_sizes, projection_dim=None, num_runs=5):
+#   models = []
+#   rmses = []
+
+#   for i in range(num_runs):
+#     model = DCN(use_cross_layer=use_cross_layer,
+#                 deep_layer_sizes=deep_layer_sizes,
+#                 projection_dim=projection_dim)
+#     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate))
+#     models.append(model)
+
+#     model.fit(cached_train, epochs=epochs, verbose=False)
+#     metrics = model.evaluate(cached_test, return_dict=True)
+#     rmses.append(metrics["RMSE"])
+
+#   mean, stdv = np.average(rmses), np.std(rmses)
+
+#   return {"model": models, "mean": mean, "stdv": stdv}
+
+epochs = 8
+learning_rate = 0.01
+
+# dcn_result = run_models(use_cross_layer=True, deep_layer_sizes=[192, 192])
+
+# dcn_lr_result = run_models(use_cross_layer=True,
+#                            projection_dim=20,
+#                            deep_layer_sizes=[192, 192])
+
+# dnn_result = run_models(use_cross_layer=False,
+#                         deep_layer_sizes=[192, 192, 192])
 
 
-# # DATA_URL = "D:/ratings.csv"
-# # df = pd.read_csv(DATA_URL)
-# # ratings = tf.data.Dataset.from_tensor_slices(dict(df)).map(lambda x: {
-# #     "user_id": str(x["user_id"]),
-# #     "item_id": str(x["item_id"]),
-# #     "rating": float(x["rating"])
-# # })
+# print("DCN            RMSE mean: {:.4f}, stdv: {:.4f}".format(
+#     dcn_result["mean"], dcn_result["stdv"]))
+# print("DCN (low-rank) RMSE mean: {:.4f}, stdv: {:.4f}".format(
+#     dcn_lr_result["mean"], dcn_lr_result["stdv"]))
+# print("DNN            RMSE mean: {:.4f}, stdv: {:.4f}".format(
+#     dnn_result["mean"], dnn_result["stdv"]))
 
+
+model = DCN(use_cross_layer=False,
+                deep_layer_sizes=[192,192],
+                projection_dim=20)
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate))
+
+model.fit(cached_train, epochs=epochs, verbose=False)
+
+# # Get weight
+result = model({
+    "PlanName": np.array(["Solutions 500"]),
+    "eemGender": np.array(['M']),
+    "eemState": np.array(["WA"])
+})
+print(f"result: {result}")
+
+# tf.saved_model.save(model, "export")
+# model.save("export")
+# tf.keras.models.save_model(model, "keras_export")
+# tfjs.converters.save_keras_model(model, "js-export")
